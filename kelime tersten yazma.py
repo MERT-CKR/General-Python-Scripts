b627bd7ff82f250a13aa6f78f13a355d7eed77bc
Mert Çakır <82875825+MERT-CKR@users.noreply.github.com>
2023-05-20 12:50:21 +0300

    Add files via upload

---
+++ kelime tersten yazma.py
@@ -0,0 +1,4 @@
+kelime = input("kelimeyi veya metni girin: ")
+print(kelime[::-1])
+
+
--- türkçe duyarlı metindeki kelime sayısı.py
+++ türkçe duyarlı metindeki kelime sayısı.py
@@ -1,7 +1,22 @@
-kelime =input("kelimeyi giriniz: ")
-metin = input("metni giriniz: ")
-print("büyük harf küçük harf duyarlılığı vardır")
+import unicodedata
 
-sayac = metin.count(kelime)
-print("metin içinde {} kelimesi {} defa geçmektedir".format(kelime,sayac))
+def normalize_turkish(text):
+    text_normalized = unicodedata.normalize('NFKC', text)
+    text_normalized = text_normalized.replace("İ", "i")
+    text_normalized = text_normalized.casefold()
+    return text_normalized
 
+kelime = input("Kelimeyi girin: ")
+
+metin = input("metni girin:")
+
+islem = input("Büyük harf küçük harf duyarlılığı olsun mu? (Evet/Hayır): ")
+
+if islem.lower() == "evet" or islem == "1":
+    sayac = metin.count(kelime)
+else:
+    metin = normalize_turkish(metin)
+    kelime = normalize_turkish(kelime)
+    sayac = metin.count(kelime)
+
+print(f"Metin içinde '{kelime}' kelimesi {sayac} defa geçmektedir.")
