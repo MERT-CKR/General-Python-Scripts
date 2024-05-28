11b7a84fe3930c88b999fd293159ea65437f5683
Mert Çakır <82875825+MERT-CKR@users.noreply.github.com>
2024-05-28 15:49:07 +0300

    Update pisagor.py

--- pisagor.py
+++ pisagor.py
@@ -1,5 +1,6 @@
-def pisagor():
-    a = float(input("Kenar1: "))
-    b = float(input("Kenar2: "))
+a = float(input("Kenar1: "))
+b = float(input("Kenar2: "))
+
+def pisagor(a,b):
     print("pisagor: ",(a**2 + b**2)**0.5)
-pisagor()
+pisagor(a,b)
